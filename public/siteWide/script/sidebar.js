9d947092566fbca02f658e92b3dc6864cec4dcd0
Kaleb Afseth <afseth598@gmail.com>
2020-11-04 15:26:59 -0800

    Trimming Style Sheets & Text Fitting
    Removed some repetitious styling, so that style changes only appear once
    
    Implemented a fit to container script for the profile username

--- public/siteWide/script/sidebar.js
+++ public/siteWide/script/sidebar.js
@@ -1,35 +1,30 @@
 $(document).ready(function () {
     let isPortrait = window.matchMedia("(max-aspect-ratio: 1)");
-    let itemWidth;
+    let itemSize = $("#navContainer a").outerWidth();
+    let sidebarSize;
 
-    initialize();
-    function initialize() {
-        updateNavGrid();
-        onResize();
+    // Initial Calls
+    hideScrollBar();
+    updateNavGrid();
+    onResize();
+    fitUsername();
 
-        // Adaptive Listeners
-        isPortrait.addListener(updateNavGrid);
-        window.addEventListener("resize", onResize);
-        $("#dropdownIcon").mouseenter(hoverDropdown); //mouseleave is declared alongside the .addClass point.
-
-        hideScrollBar();
-
-        /* Ratio Profile Text */
-        $("#profile").css("font-size", parseFloat($("#profile").css("height")) * .2);
-
-    }
+    // Adaptive Listeners
+    isPortrait.addListener(updateNavGrid);
+    window.addEventListener("resize", onResize);
+    $("#dropdownIcon").mouseenter(hoverDropdown); //mouseleave is declared alongside the .addClass point.
 
     function updateNavGrid() {
         if (isPortrait.matches) {
-            itemWidth = parseFloat($("#navContainer").css("height")) * .75;
-            let section = itemWidth + "px "
+            let section = itemSize + "px "
             $("#navContainer>nav").css("grid-template-columns", section + section + section + section + section);
             $("#navContainer>nav").css("grid-template-rows", "");
+
         } else {
-            itemWidth = parseFloat($("#navContainer").css("width")) * .75;
-            let section = itemWidth + "px "
+            let section = itemSize + "px "
             $("#navContainer>nav").css("grid-template-columns", "");
             $("#navContainer>nav").css("grid-template-rows", section + section + section + section + section);
+
         }
     }
 
@@ -42,33 +37,39 @@ $(document).ready(function () {
             $("#dropdownIcon").css("display", "none");
             $("#navContainer>nav").css("display", "grid");
         }
+        if (isPortrait.matches) {
+            sidebarSize = parseFloat($("#sidebar").css("height"));
+        } else {
+            sidebarSize = parseFloat($("#sidebar").css("width"));
+        }
     }
 
     function navHasGap() {
-        let paddingMargin = 40;
         if (isPortrait.matches) {
             let columnString = $("#sidebar").css("grid-template-columns");
-            let secondColumnSize = parseFloat(columnString.split(" ")[1]) - paddingMargin;
-            return secondColumnSize - (itemWidth * 5) < 0;
+            let secondColumnSize = parseFloat(columnString.split(" ")[1]);
+            return secondColumnSize - (itemSize * 5) < 0;
         } else {
             let rowString = $("#sidebar").css("grid-template-rows");
-            let secondRowSize = parseFloat(rowString.split(" ")[1]) - paddingMargin;
-            return secondRowSize - (itemWidth * 5) < 0;
+            let secondRowSize = parseFloat(rowString.split(" ")[1]);
+            return secondRowSize - (itemSize * 5) < 0;
         }
     }
 
     function hideScrollBar() {
         $("main").css("overflow", "scroll");
-        let mainMargin = parseFloat($("main").css("margin"));
-        let mainPadding = parseFloat($("main").css("padding"));
         $("main").css("margin-right", -100 + "px");
         $("main").css("padding-right", 100 + "px");
         $("main").css("margin-bottom", -100 + "px");
         $("main").css("padding-bottom", 100 + "px");
+        if (isPortrait.matches) {
+            $("main").css("height", "calc(100% - " + sidebarSize);
+        } else {
+            $("main").css("height", "100%")
+        }
     }
 
-    function hoverDropdown(isHovered){
-        console.log(isHovered);
+    function hoverDropdown(isHovered) {
         if (isHovered.type == "mouseenter") {
             $("#navContainer>nav").css("display", "grid");
             $("#navContainer>nav").addClass("dropdownExpanded");
@@ -78,4 +79,24 @@ $(document).ready(function () {
             $("#navContainer>nav").removeClass("dropdownExpanded");
         }
     }
+
+    function fitUsername() {
+        let fontWeight = $("#profile").css("font-weight");
+        let fontSize = $("#profile").css("font-size");
+        let fontFamily = $("#profile").css("font-family");
+        let font = fontWeight + " " + fontSize + " " + fontFamily;
+        let text = $("#profile>span").text();
+
+        // The following code block is adapted from: https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
+        var canvas = fitUsername.canvas || (fitUsername.canvas = document.createElement("canvas"));
+        var context = canvas.getContext("2d");
+        context.font = font;
+        var metrics = context.measureText(text);
+        let textWidth = metrics.width;
+
+        let containerSize = parseFloat($("#profile").css("width"));
+        let ratio = containerSize / textWidth;
+
+        $("#profile>span").css("font-size", ratio +"em");
+    };
 });
